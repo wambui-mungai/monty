e9d2486e089b2ab57decce7817cb2f093e05a5f0
Benard Ronoh <biikate48@gmail.com>
2023-06-23 04:49:43 -0700

    pall.c

--- pall.c
+++ pall.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * f_pall - prints stack
+ * f_pall - prints the stack
  *
- * @head: stack head.
- * @counter: no used.
- *
- * Return: no return.
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
 */
 void f_pall(stack_t **head, unsigned int counter)
 {
