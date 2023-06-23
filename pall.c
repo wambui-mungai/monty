2e37811be2f5ae0ee8aff4e2a3a21dd3b07d59af
Benard Ronoh <biikate48@gmail.com>
2023-06-22 17:09:57 -0700

    bugs fixing

--- pall.c
+++ pall.c
@@ -5,20 +5,21 @@
  * f_pall - prints stack
  *
  * @head: stack head.
- * @counter: no used.
+ * @counter: not used.
  * Return: no return.
-*/
+ */
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
+    stack_t *h;
+    (void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+    h = *head;
+    if (h == NULL)
+        return;
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->prev;
+    }
 }
+
