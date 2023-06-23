a2623b3a1088b8a05870ab5f71f8cb85add44555
Benard Ronoh <biikate48@gmail.com>
2023-06-23 04:52:49 -0700

    pint.c

--- pint.c
+++ pint.c
@@ -1,24 +1,20 @@
 #include "monty.h"
-#include <stdio.h>
-
 /**
- * f_pall - prints stack
+ * f_pint - prints the top
  *
- * @head: stack head.
- * @counter: no used.
- * Return: no return.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
 */
-void f_pall(stack_t **head, unsigned int counter)
+void f_pint(stack_t **head, unsigned int counter)
 {
-        stack_t *h;
-        (void)counter;
-
-        h = *head;
-        if (h == NULL)
-                return;
-        while (h)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-        }
-}}
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
